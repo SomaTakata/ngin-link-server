ef871a1851dcc016bedd7405317389ab1a0f22fd
rare0b <rare0b0110@gmail.com>
2024-02-23 23:23:54 +0900

    json定義

--- internal/api/model/ngin_link.go
+++ internal/api/model/ngin_link.go
@@ -1,11 +1,11 @@
 package model
 
 type NginLink struct {
-	NginLinkID string
-	Links      []Link
+	NginLinkID string `json:"ngin_link_id"`
+	Links      []Link `json:"links"`
 }
 
 type Link struct {
-	PlatformName string
-	LinkURL      string
+	PlatformName string `json:"platform_name"`
+	LinkURL      string `json:"link_url"`
 }
